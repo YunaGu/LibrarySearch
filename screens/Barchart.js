3d9a4e380c519ebff520717c3495290e55cfa3aa
Yun Gu <guy36@cardiff.ac.uk>
2023-07-20 23:52:53 +0100

    change to function

--- screens/Barchart.js
+++ screens/Barchart.js
@@ -1,7 +1,7 @@
 import { SvgChart, SVGRenderer } from '@wuba/react-native-echarts';
 import * as echarts from 'echarts/core';
 import { useRef, useEffect } from 'react';
-// import {Dimensions} from 'react-native';
+
 import {
   BarChart,
 } from 'echarts/charts';
@@ -70,16 +70,68 @@ export default function App() {
         show: false
       }
     },
-    series: [
-      {
-        data: [0,0,0,0,0,0,0,5,10,40,{value: 70, itemStyle:{color: '#045BC6'}},60,50,55,70,60,30,20,10,3,0,0,0,0],
-        type: 'bar',
-      },
-    ],
+    // series: [
+    //   {
+    //     data: [0,0,0,0,0,0,0,5,10,40,{value: 70, itemStyle:{color: '#045BC6'}},60,50,55,70,60,30,20,10,3,0,0,0,0],
+    //     type: 'bar',
+    //     emphasis: {
+    //       itemStyle: {
+    //         color: '#045BC6', // Color when a bar is in emphasis (selected)
+    //       },
+    //     },
+    //   },
+    // ],
     color: [
       // '#045BC6',
       'background: rgba(4, 91, 198, 0.50)'
     ],
+    series: [
+      {
+        data: [0,0,0,0,0,0,0,5,10,40,{value: 70, itemStyle:{color: '#045BC6'}},60,50,55,70,60,30,20,10,3,0,0,0,0],
+        type: 'bar',
+        emphasis: {
+          itemStyle: {
+            color: '#045BC6', // Color when a bar is in emphasis (selected)
+          },
+        },
+        // Add the 'click' event to handle clicks on the bar chart
+        // 'params' object contains information about the clicked item
+        // Use 'params.dataIndex' to get the index of the clicked column
+        // and update the state accordingly
+        // 'dispatchAction' is used to set the emphasis (selected) state on the clicked bar
+        // Finally, update the 'forecastData' state with the selected forecast information
+        // and call 'setOption' to apply the updated option to the chart
+        onclick: (params) => {
+          const clickedIndex = params.dataIndex;
+          setSelectedColumnIndex(clickedIndex);
+
+          // Update the 'forecastData' state based on the selected column
+          if (clickedIndex >= 0 && clickedIndex < forecastData.data.length) {
+            setForecastData(forecastData.data[clickedIndex]);
+          } else {
+            setForecastData({ time: 'Tap hours for forecasts', value: '' });
+          }
+
+          // Highlight the clicked bar
+          const chart = params.event?.getChart();
+          chart?.dispatchAction({
+            type: 'highlight',
+            dataIndex: clickedIndex,
+          });
+
+          // Remove highlight from other bars
+          for (let i = 0; i < option.series[0].data.length; i++) {
+            if (i !== clickedIndex) {
+              chart?.dispatchAction({
+                type: 'downplay',
+                dataIndex: i,
+              });
+            }
+          }
+        },
+      },
+    ],
+
   }
   return <ChartComponent option={option} />
 }
--- screens/Occupancy.js
+++ screens/Occupancy.js
@@ -1,139 +1,131 @@
-import { Text, View, StyleSheet, Image } from 'react-native'
-import React, { Component } from 'react'
+import React, { useState } from 'react';
+import { Text, View, StyleSheet, Image } from 'react-native';
 import People from "../assets/people.png";
-import Icon from 'react-native-vector-icons/Ionicons';
-// import Barchart from './Barchart'
+// import Icon from 'react-native-vector-icons/Ionicons';
 import Barchart from './Barchart';
 import BottomModal from './BottomModal';
 
-export default class Occupancy extends Component {
-  render() {
-    return (
-        <View style={styles.compWrapper}>
+const Occupancy = () => {
+  const [realTimeOccupancy, setRealTimeOccupancy] = useState(70);
 
-        <View style={styles.titleWrapper}>
-          <View style={styles.firstTwo}>
-            <Image source={People} style={{height: 13, width: 18}}></Image>
-            <Text style={styles.textBodyBold}>Occupancy rate</Text>
-          </View>
-            {/* <Icon name="chevron-down-outline" size={24} color='#383735'></Icon> */}
+  const [selectedColumnIndex, setSelectedColumnIndex] = useState(-1);
+  const [realTimeData, setRealTimeData] = useState({ time: '11 am', value: 70 });
+  const [forecastData, setForecastData] = useState({ time: 'Tap hours for forecasts', value: '' });
+
+  return (
+    <View style={styles.compWrapper}>
+      <View style={styles.titleWrapper}>
+        <View style={styles.firstTwo}>
+          <Image source={People} style={{ height: 13, width: 18 }}></Image>
+          <Text style={styles.textBodyBold}>Occupancy rate</Text>
         </View>
-
-        {/* <View style={styles.border}>
-            <Text style={styles.textBodyDefault}>Ground floor</Text>
-            <Icon name="chevron-down-outline" size={14} color='#383735'></Icon>
-        </View> */}
-
-        <BottomModal />
-        
-        <View style={styles.rates}>
-            <View style={styles.realTime}>
-                <Text style={styles.textBodyDefault}>Real time: 11 am - </Text>
-                <Text style={[styles.textBodyDefault, styles.textBlue]}>70%</Text>
-            </View>
-            
-            {/* <View style={styles.realTime}>
-                <Text style={styles.textBodyDefault}>Real time: 11 am - </Text>
-                <Text style={[styles.textBodyDefault, styles.textBlue]}>70%</Text>
-            </View> */}
-
-            <View style={styles.forecast}>
-                <Text style={[styles.textBodyDefault, styles.textGray]}>Tap hours for forecasts</Text>
-            </View>
-        </View>
-
-
-        {/* Bar chart */}
-        <View style={styles.barChart}>
-            <Barchart />
-        </View>
-
+        {/* <Icon name="chevron-down-outline" size={24} color='#383735'></Icon> */}
       </View>
-    )
-  }
-}
+
+      <BottomModal />
+
+      <View style={styles.rates}>
+        <View style={styles.realTime}>
+          <Text style={styles.textBodyDefault}>Real time: {realTimeData.time} - </Text>
+          <Text style={[styles.textBodyDefault, styles.textBlue]}>{realTimeData.value}%</Text>
+        </View>
+
+        <View style={styles.forecast}>
+          <Text style={[styles.textBodyDefault, styles.textGray]}>Tap hours for forecasts</Text>
+        </View>
+      </View>
+
+      {/* Bar chart */}
+      <View style={styles.barChart}>
+        <Barchart />
+      </View>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    barChart:{
-        height: 115,
-        width: 330,
-        // backgroundColor:'red',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    forecast:{
-        marginLeft: 38,
-    },
-    realTime:{
-        display: 'flex',
-        flexDirection: 'row',
-        marginLeft: 38
-    },
-    textGray:{
-        color: '#666'
-    },
-    textBlue:{
-        color: '#045BC6'
-    },
-    border:{
-        borderColor: 'rgba(0, 0, 0, 0.20)',
-        borderWidth: 1,
-        borderRadius: 6,
+  barChart: {
+    height: 115,
+    width: 330,
+    // backgroundColor:'red',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  forecast: {
+    marginLeft: 38,
+  },
+  realTime: {
+    display: 'flex',
+    flexDirection: 'row',
+    marginLeft: 38
+  },
+  textGray: {
+    color: '#666'
+  },
+  textBlue: {
+    color: '#045BC6'
+  },
+  border: {
+    borderColor: 'rgba(0, 0, 0, 0.20)',
+    borderWidth: 1,
+    borderRadius: 6,
 
-        width: 135, //how to hug content?
-        paddingTop: 5,
-        paddingBottom: 5,
-        paddingLeft: 10,
-        paddingRight: 10,
+    width: 135, //how to hug content?
+    paddingTop: 5,
+    paddingBottom: 5,
+    paddingLeft: 10,
+    paddingRight: 10,
 
-        gap: 10,
+    gap: 10,
 
-        display: 'inline-flex',
-        flexDirection: 'row',
-        alignItems: 'center',
+    display: 'inline-flex',
+    flexDirection: 'row',
+    alignItems: 'center',
 
-        marginLeft: 38,
-    },
-    firstTwo:{
-        display: 'flex',
-        flexDirection: 'row',
-        alignItems: 'center',
-        gap: 20,
-      },
-      titleWrapper:{
-        display: 'flex',
-        flexDirection: 'row',
-        width: '100%',
-        alignItems: 'center',
-        justifyContent: 'space-between'
-      },
-      compWrapper:{
-        display: 'flex',
-        width: '100%',
-        paddingTop: 10,
-        paddingBottom: 10,
-        paddingLeft: 30,
-        paddingRight: 30,
-        flexDirection: 'column',
-        alignItems: 'flex-start',
-        gap: 10,
-    },
-    textBodyBold:{
-        color: "#383735",
-        // fontFamily: 'Franklin Gothic Book',
-        fontSize: 17,
-        lineHeight: 24,
-        // fontStyle: 'normal',
-        fontWeight: '600',
-        textAlign: "left",
-    },
-    textBodyDefault:{
-        color: "#383735",
-        // fontFamily: 'Franklin Gothic Book',
-        fontSize: 17,
-        lineHeight: 24,
-        // fontStyle: 'normal',
-        fontWeight: '400',
-        textAlign: "left",
-    },
-})
+    marginLeft: 38,
+  },
+  firstTwo: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 20,
+  },
+  titleWrapper: {
+    display: 'flex',
+    flexDirection: 'row',
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'space-between'
+  },
+  compWrapper: {
+    display: 'flex',
+    width: '100%',
+    paddingTop: 10,
+    paddingBottom: 10,
+    paddingLeft: 30,
+    paddingRight: 30,
+    flexDirection: 'column',
+    alignItems: 'flex-start',
+    gap: 10,
+  },
+  textBodyBold: {
+    color: "#383735",
+    // fontFamily: 'Franklin Gothic Book',
+    fontSize: 17,
+    lineHeight: 24,
+    // fontStyle: 'normal',
+    fontWeight: '600',
+    textAlign: "left",
+  },
+  textBodyDefault: {
+    color: "#383735",
+    // fontFamily: 'Franklin Gothic Book',
+    fontSize: 17,
+    lineHeight: 24,
+    // fontStyle: 'normal',
+    fontWeight: '400',
+    textAlign: "left",
+  },
+});
+
+export default Occupancy;
